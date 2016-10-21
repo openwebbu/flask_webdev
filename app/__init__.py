87daea4a8a13ff322c02dec7b8f2dc5d47bbc87b
Lawrence Luo <luolawrence1@gmail.com>
2016-10-21 00:47:12 -0400

    updated starter code

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,10 @@
-from flask import Flask
+# ---import Flask library and class---
 # from flask.ext.sqlalchemy import SQLAlchemy # database interfacing
 
-app_obj = Flask(__name__) # Initialize Flask web application object
+# ---Initialize Flask web application object---
 # app_obj.config.from_object('config') # use database path
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+# app_obj.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 # db = SQLAlchemy(app_obj) # create database
 
-from app import views # This is the app folder in which views are located
+# ---Import views.py file from app folder---
 # from app import models
